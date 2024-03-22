94a9c67e8b33c58f644c4af2b0617a4977904393
Sven Brodny <svenbrodny@googlemail.com>
2024-03-23 00:03:15 +0100

    refactor: app component

--- apps/client/src/app/app.component.spec.ts
+++ apps/client/src/app/app.component.spec.ts
@@ -1,27 +1,67 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { Location } from '@angular/common';
+import {
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 
+import { AppComponent } from './app.component';
+import { routes } from './app.routes';
+
 describe('AppComponent', () => {
+  let location: Location;
+  let router: Router;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent, RouterTestingModule],
+      imports: [AppComponent, RouterTestingModule.withRoutes(routes)],
     }).compileComponents();
+
+    router = TestBed.get(Router);
+    location = TestBed.get(Location);
+
+    fixture = TestBed.createComponent(AppComponent);
+    router.initialNavigation();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  /* Class Tests */
+  it('should start with "/" route', fakeAsync(() => {
+    // Arrange
+    const path = location.path();
+
+    // Act
+    tick();
+
+    // Assert
+    expect(path).toBe('/');
+  }));
+
+  /* DOM Tests */
+  it('should display header, router and footer', () => {
+    // Arrange
+    const header = fixture.nativeElement.querySelector('header');
+    const router = fixture.nativeElement.querySelector('router-outlet');
+    const footer = fixture.nativeElement.querySelector('footer');
+
+    // Act
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome client'
-    );
+
+    // Assert
+    expect(header).toBeTruthy();
+    expect(router).toBeTruthy();
+    expect(footer).toBeTruthy();
   });
 
-  it(`should have as title 'client'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('client');
-  });
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   expect(compiled.querySelector('h1')?.textContent).toContain(
+  //     'Welcome client'
+  //   );
+  // });
 });
--- apps/client/src/app/app.config.ts
+++ apps/client/src/app/app.config.ts
@@ -1,7 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
+
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [provideRouter(routes)],
 };
