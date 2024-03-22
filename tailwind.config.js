dd25290580d03e088aa9e4733ae820ff211119ba
Sven Brodny <svenbrodny@googlemail.com>
2024-03-22 23:56:37 +0100

    build: add theme vars in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'fb-color-black-1': 'var(--fb-color-black-1)',
+        'fb-color-black-2': 'var(--fb-color-black-2)',
+        'fb-color-white': 'var(--fb-color-white)',
+        'fb-color-green-1': 'var(--fb-color-green-1)',
+        'fb-color-green-2': 'var(--fb-color-green-2)',
+        'fb-color-text-1': 'var(--fb-color-text-2)',
+        'fb-color-text-2': 'var(--fb-color-text-2)',
+
+        'fb-font-size-title': 'var(--fb-font-size-title)',
+        'fb-font-size-subtitle-1': 'var(--fb-font-size-subtitle-1)',
+        'fb-font-size-subtitle-2': 'var(--fb-font-size-subtitle-2)',
+        'fb-font-size-body-1': 'var(--fb-font-size-body-1)',
+        'fb-font-size-body-2': 'var(--fb-font-size-body-2)',
+        'fb-font-size-small': 'var(--fb-font-size-small)',
+      },
+      spacing: {
+        'fb-padding-1': 'var(--fb-padding-1)',
+        'fb-padding-2': 'var(--fb-padding-2)',
+        'fb-padding-3': 'var(--fb-padding-3)',
+      },
+    },
   },
-  plugins: [],
 };
