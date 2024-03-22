55122ec1256b95171a107bf6000a167294361592
Sven Brodny <svenbrodny@googlemail.com>
2024-03-22 14:35:34 +0100

    build(api): add mongodb init method

---
+++ apps/api/src/helper/db.helper.ts
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+export class DBHelper {
+  static init(): void {
+    const username = process.env.MONGODB_USER;
+    const password = process.env.MONGODB_PASSWORD;
+    const cluster = process.env.MONGODB_CLUSTER;
+    const database = process.env.MONGODB_DATABASE;
+
+    const params = Object.values({
+      allowAutoRetry: 'retryWrites=true',
+      writeConcern: 'w=majority',
+    }).join('&');
+
+    const uri = `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${database}?${params}`;
+
+    mongoose.connect(uri);
+    mongoose.connection
+      .once('connected', () => {
+        console.info(
+          `[server]: Connected to database '${mongoose.connection.host}'`
+        );
+        // callback();
+      })
+      .once('reconnected', () => {
+        console.info(
+          `[server]: Reconnected to database '${mongoose.connection.host}'`
+        );
+      })
+      .on('disconnected', () => {
+        console.warn(
+          `[server]: Disconnected from database '${mongoose.connection.host}'`
+        );
+      })
+      .on('error', (err) => {
+        console.warn(`[server]: Connection error: '${err}'`);
+      });
+  }
+}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,19 +3,25 @@
  * This is only a minimal backend to get started.
  */
 
+import cors from 'cors';
 import express from 'express';
-import * as path from 'path';
+import { DBHelper } from './helper/db.helper';
 
 const app = express();
+// app.use(express.json());
+app.use(cors());
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+// app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to api!' });
 });
 
-const port = process.env.PORT || 3333;
+const port = process.env.PORT;
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
+
 server.on('error', console.error);
+
+DBHelper.init();
