c21407f46f0fe436a635972999ff82abf163ff4b
Sven Brodny <svenbrodny@googlemail.com>
2024-03-24 14:15:58 +0100

    test(start): fix test add missing router module

--- apps/client/src/app/modules/start/start.component.spec.ts
+++ apps/client/src/app/modules/start/start.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { StartComponent } from './start.component';
 
@@ -8,7 +9,7 @@ describe('StartComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [StartComponent],
+      imports: [StartComponent, RouterTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(StartComponent);
