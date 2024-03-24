7950fa1593c6fc6b1b26f663de0c958f73e8f4c8
Sven Brodny <svenbrodny@googlemail.com>
2024-03-24 14:00:00 +0100

    test(header): fix tests & update component

--- apps/client/src/app/components/header/header.component.spec.ts
+++ apps/client/src/app/components/header/header.component.spec.ts
@@ -1,60 +1,88 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Location } from '@angular/common';
+import {
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
 
-import { LeagueService, MockLeagueService, mockLeague } from '../../services';
+import { routes } from '../../app.routes';
+import { LeagueService, mockLeague } from '../../services';
 
-import { HeaderComponent } from './header.component';
+import { HeaderComponent, SELECTED_LEAGUE_DEFAULT } from './header.component';
 
 describe('HeaderComponent', () => {
   let fixture: ComponentFixture<HeaderComponent>;
+  let component: HeaderComponent;
   let leagueService: LeagueService;
+  let location: Location;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent],
-      providers: [{ provide: LeagueService, useClass: MockLeagueService }],
+      imports: [RouterTestingModule.withRoutes(routes), HeaderComponent],
+      providers: [LeagueService],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
     leagueService = TestBed.inject(LeagueService);
+    location = TestBed.inject(Location);
     fixture.detectChanges();
   });
 
   /* Class Tests */
-  it('should display progress bar while loading', () => {
-    const component = fixture.componentInstance;
-    expect(component.progressMode())
-      .withContext('off at first')
-      .toBe('determinate');
+  it('should set form control properly', fakeAsync(() => {
+    // given
+    expect(component.leagueControl.value).toBe(SELECTED_LEAGUE_DEFAULT);
 
-    component.isLoading.set(true);
-    expect(component.progressMode())
-      .withContext('on loading')
-      .toBe('indeterminate');
+    // when
+    leagueService.setSelectedLeague(mockLeague);
+    tick();
 
-    component.isLoading.set(false);
-    expect(component.progressMode())
-      .withContext('off after loading')
-      .toBe('determinate');
-  });
-
-  it('should set selectedLeague to undefined on logo click', () => {
-    leagueService.selectedLeague.set(mockLeague);
-    expect(leagueService.selectedLeague()).toBe(mockLeague);
-
-    const debugElement = fixture.debugElement;
-    const logo = debugElement.query(By.css('futbet-logo'));
-    logo.triggerEventHandler('click', null);
-
-    expect(leagueService.selectedLeague()).toBe(undefined);
-  });
+    // then
+    expect(component.leagueControl.value).toBe(mockLeague.url);
+  }));
 
   /* DOM Tests */
   it(`should display 'futbet' logo`, () => {
+    // given
     const debugElement = fixture.debugElement;
+
+    // when
     const logo = debugElement.query(By.css('futbet-logo'));
 
+    // then
     expect(logo).toBeTruthy();
-    expect(logo.nativeElement.textContent).toEqual('futbet');
+    expect(logo.nativeElement.textContent).toEqual('FUT⚽BET');
   });
+
+  it('should route to start on logo-toggle click', fakeAsync(() => {
+    // given
+    const initPath = '/leagues/' + mockLeague.url;
+    location.go(initPath);
+    expect(location.path()).toEqual(initPath);
+
+    // when
+    const button = fixture.debugElement.query(By.css(`.logo-toggle`));
+    button.nativeElement.click();
+    tick();
+
+    // then
+    expect(location.path()).toEqual('/');
+  }));
+
+  it('should route to league on league-toggle click', fakeAsync(() => {
+    // given
+    expect(location.path()).toEqual('');
+
+    // when
+    const button = fixture.debugElement.query(By.css(`.${mockLeague.url}`));
+    button.nativeElement.click();
+    tick();
+
+    // then
+    expect(location.path()).toEqual('/leagues/' + mockLeague.url);
+  }));
 });
