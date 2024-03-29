149c83ffeaad96340e5aa94842737021cb2cbb3d
Sven Brodny <svenbrodny@googlemail.com>
2024-03-29 18:05:20 +0100

    feat(app): loading spinner during router outlet initialization

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  DestroyRef,
+  ViewChild,
+  inject,
+  signal,
+} from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { RouterModule, RouterOutlet } from '@angular/router';
+
 import { FooterComponent, HeaderComponent } from './components';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, HeaderComponent, FooterComponent],
+  imports: [
+    RouterModule,
+    MatProgressSpinnerModule,
+    HeaderComponent,
+    FooterComponent,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'futbet-root',
   styles: `
@@ -18,15 +34,29 @@ import { FooterComponent, HeaderComponent } from './components';
     header { @apply flex bg-fb-color-green-1 p-fb-padding-3 pb-0; }
     main { @apply flex bg-fb-color-green-1-light p-fb-padding-3; }
     footer { @apply flex justify-center px-fb-padding-3 py-10; }
+    mat-spinner { @apply mx-auto my-10; }
   `,
   template: `
     <header></header>
 
     <main>
       <router-outlet />
+      @if (isLoading()) { <mat-spinner /> }
     </main>
 
     <footer></footer>
   `,
 })
-export class AppComponent {}
+export class AppComponent implements AfterViewInit {
+  @ViewChild(RouterOutlet)
+  private outlet!: RouterOutlet;
+
+  readonly isLoading = signal<boolean>(true);
+  readonly destroyRef = inject(DestroyRef);
+
+  public ngAfterViewInit(): void {
+    this.outlet.activateEvents
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(() => this.isLoading.set(false));
+  }
+}
