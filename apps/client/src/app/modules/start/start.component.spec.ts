2d65f2599a627da1361f1163a16dbd1cd25ee239
Sven Brodny <svenbrodny@googlemail.com>
2024-03-29 16:02:35 +0100

    test: fix header & start components

--- apps/client/src/app/modules/start/start.component.spec.ts
+++ apps/client/src/app/modules/start/start.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 
+import { routes } from '../../app.routes';
 import { StartComponent } from './start.component';
 
 describe('StartComponent', () => {
@@ -9,7 +11,11 @@ describe('StartComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [StartComponent, RouterTestingModule],
+      imports: [
+        StartComponent,
+        RouterTestingModule.withRoutes(routes),
+        BrowserAnimationsModule,
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(StartComponent);
