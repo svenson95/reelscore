63ff95cebed86e6c0be429202a2de549be07cfd8
Sven Brodny <svenbrodny@googlemail.com>
2024-05-19 16:45:22 +0200

    refactor(router): service & view component

--- apps/client/src/app/services/route.service.ts
+++ apps/client/src/app/services/route.service.ts
@@ -1,17 +1,26 @@
-import { Location } from '@angular/common';
 import { Injectable, computed, inject } from '@angular/core';
-
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs';
 import { LeagueUrl } from '../constants';
 
 @Injectable()
 export class RouteService {
-  private readonly location = inject(Location);
+  private readonly router = inject(Router);
+
+  private readonly url = toSignal<string>(
+    this.router.events.pipe(
+      takeUntilDestroyed(),
+      filter((event) => event instanceof NavigationEnd),
+      map((event) => (event as NavigationEnd).url)
+    )
+  );
 
   readonly activeRoute = computed<LeagueUrl>(() => {
-    const url = this.location.path();
+    const url = this.url();
+    if (url === undefined) throw new Error('route is undefined');
     const leagueUrlIndex = url.indexOf('/', 2) + 1;
-    const path = url.substring(leagueUrlIndex, url.length);
-    const result = url !== undefined ? path : '/';
-    return result as LeagueUrl;
+    const leagueUrl = url.substring(leagueUrlIndex, url.length);
+    return leagueUrl as LeagueUrl;
   });
 }
