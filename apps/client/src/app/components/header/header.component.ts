663ea8ee477561d75855fac4c06f31d1fa7c9d2b
Sven Brodny <svenbrodny@googlemail.com>
2024-05-20 19:38:47 +0200

    refactor(styles): header components

--- apps/client/src/app/components/header/header.component.ts
+++ apps/client/src/app/components/header/header.component.ts
@@ -1,7 +1,6 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  ViewEncapsulation,
   computed,
   inject,
 } from '@angular/core';
@@ -16,49 +15,6 @@ import { LeagueSelectComponent } from './league-select.component';
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [LeagueSelectComponent, LeagueSelectMobileComponent],
-  encapsulation: ViewEncapsulation.None,
-  styles: `
-    header {
-      mat-button-toggle.mat-button-toggle {
-        &:not(.logo-toggle) .mat-button-toggle-label-content {
-          font-size: var(--fb-font-size-small);
-          // material class height change (1)
-          line-height: calc(var(--fb-size-league-select-height) - 10px);
-        }
-      }
-
-      .mat-mdc-form-field-type-mat-select {
-        .mdc-text-field--filled:not(.mdc-text-field--disabled) {
-          @apply bg-transparent;
-        }
-
-        .mat-mdc-floating-label mat-label {
-          @apply opacity-50;
-        }
-
-        .mat-mdc-form-field-subscript-wrapper {
-          display: none;
-        }
-
-        .mat-mdc-select-arrow {
-          @apply opacity-50;
-        }
-
-        &.mat-focused .mat-mdc-select-arrow {
-          @apply opacity-50;
-        }
-
-        .mdc-text-field--filled .mdc-line-ripple::before {
-          border-bottom-width: 0;
-        }
-
-        .mdc-text-field--filled:not(.mdc-text-field--disabled)
-          .mdc-line-ripple::after {
-          border-bottom-color: var(--fb-color-green-1);
-        }
-      }
-    }
-  `,
   template: `
     <futbet-header-league-select />
     @if (isMobile()) {
