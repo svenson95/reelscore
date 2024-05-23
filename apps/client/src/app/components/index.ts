086f13ed7232d243b73294621de9d65bb0e53513
Sven Brodny <svenbrodny@googlemail.com>
2024-05-23 13:44:44 +0200

    feat(optimized-image): component

--- apps/client/src/app/components/index.ts
+++ apps/client/src/app/components/index.ts
@@ -2,3 +2,4 @@ export * from './date-bar/date-bar.component';
 export * from './footer/footer.component';
 export * from './header/header.component';
 export * from './logo/logo.component';
+export * from './optimized-image/optimized-image.component';
