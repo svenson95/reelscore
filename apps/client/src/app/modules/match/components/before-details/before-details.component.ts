4a204fd5a7c33f89c83804e4a368a8b7fc244b83
Sven Brodny <svenbrodny@googlemail.com>
2024-05-23 14:49:55 +0200

    feat(match): page draft

---
+++ apps/client/src/app/modules/match/components/before-details/before-details.component.ts
@@ -0,0 +1,22 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'futbet-match-before-details',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [],
+  styles: ``,
+  template: `
+    <section>
+      <div>form home ... form away</div>
+      <div>performance home ... performance away</div>
+    </section>
+    <section>
+      <div>last matches home ... last matches away</div>
+    </section>
+    <section>
+      <div>table positions (regular table and home+away table)...</div>
+    </section>
+  `,
+})
+export class MatchBeforeDetailsComponent {}
