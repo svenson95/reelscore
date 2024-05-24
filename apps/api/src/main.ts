78f3103b463ff6e66fef6ac2666ba8ee61399260
Sven Brodny <svenbrodny@googlemail.com>
2024-05-24 17:28:38 +0200

    revert(api): cors origin array (2)

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: 'https://futbet.vercel.app',
+    origin: ['http://localhost:4200', 'https://futbet.vercel.app'],
   })
 );
 app.use(helmet());
