f5dd9345a253346553aaaa3602c27a4298343dcb
Sven Brodny <svenbrodny@googlemail.com>
2024-05-24 17:05:09 +0200

    fix(api): add localhost to cors origin array

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: 'https://futbet.vercel.app',
+    origin: ['https://futbet.vercel.app', 'http://localhost:4200'],
   })
 );
 app.use(helmet());
