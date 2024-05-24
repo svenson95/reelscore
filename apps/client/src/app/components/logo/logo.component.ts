87b01a20e4a04b2de181c9238418514147a36c59
Sven Brodny <svenbrodny@googlemail.com>
2024-05-24 19:03:16 +0200

    style(logo): mat icon & font-weight

--- apps/client/src/app/components/logo/logo.component.ts
+++ apps/client/src/app/components/logo/logo.component.ts
@@ -1,23 +1,22 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'futbet-logo',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
-  template: `
-    <span class="font-normal">FUT</span>
-    <span class="px-1">⚽</span>
-    <span class="font-bold">BET</span>
-  `,
+  imports: [MatIconModule],
   styles: `
-    span {
-      user-select: none;
-    }
-
-    span:last-child {
-      color: var(--fb-color-green-1);
-    }
+    :host { @apply select-none; }
+    :host > * { @apply align-middle; }
+    mat-icon { @apply mx-1; }
+    span { @apply font-bold; }
+    span:last-child { @apply text-fb-color-green-1; }
+  `,
+  template: `
+    <span>FUT</span>
+    <mat-icon>sports_soccer</mat-icon>
+    <span>BET</span>
   `,
 })
 export class LogoComponent {}
