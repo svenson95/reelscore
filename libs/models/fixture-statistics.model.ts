3b5b7e87a7cfe12e4b0984f175536a29f542b1e4
Sven Brodny <svenbrodny@googlemail.com>
2024-05-30 22:09:23 +0200

    build(lib): add nx lib for models (2)

--- libs/models/fixture-statistics.model.ts
+++ libs/models/fixture-statistics.model.ts
@@ -1,4 +1,4 @@
-export interface FixtureStatistics {
+export interface FixtureStatisticsDTO {
   parameters: {
     fixture: string;
   };
--- libs/models/index.ts
+++ libs/models/index.ts
@@ -1,2 +1,4 @@
+export * from './fixture-statistics.model';
 export * from './fixture.model';
 export * from './mongo-db.model';
+export * from './standings.model';
