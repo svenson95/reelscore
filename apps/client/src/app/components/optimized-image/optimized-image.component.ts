3d05c792683c1b7b19568aea7dbdebe8d79e5653
Sven Brodny <svenbrodny@googlemail.com>
2024-05-31 12:42:17 +0200

    fix(optimized-image): WIP: img fill for unknown sizes

--- apps/client/src/app/components/optimized-image/optimized-image.component.ts
+++ apps/client/src/app/components/optimized-image/optimized-image.component.ts
@@ -6,19 +6,35 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [NgOptimizedImage],
-  styles: `:host { @apply self-center; }`,
-  template: ` <img
-    [ngSrc]="source()"
-    [alt]="alternate()"
-    priority
-    [width]="width()"
-    [height]="height()"
-  />`,
+  styles: `
+    :host { @apply self-center; }
+    .fill-parent { position: relative; }
+    .fill-parent img { object-fit: contain; }
+  `,
+  template: `
+    @if (fill()) {
+    <div
+      class="fill-parent"
+      [style.height]="height() + 'px'"
+      [style.width]="width() + 'px'"
+    >
+      <img [ngSrc]="source()" [alt]="alternate()" priority fill />
+    </div>
+    } @else {<img
+      [ngSrc]="source()"
+      [alt]="alternate()"
+      priority
+      [width]="width()"
+      [height]="height()"
+    />}
+  `,
 })
 export class OptimizedImageComponent {
   source = input.required<string>();
   alternate = input.required<string>();
 
-  width = input.required<string>();
-  height = input.required<string>();
+  width = input<string>();
+  height = input<string>();
+
+  fill = input<boolean>(false);
 }
