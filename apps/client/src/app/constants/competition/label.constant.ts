21b79e4218f96d97b75f6c1d53ac6f1c31bc73d2
Sven Brodny <svenbrodny@googlemail.com>
2024-06-01 22:57:34 +0200

    fix(match-list): premier league filter

--- apps/client/src/app/constants/competition/label.constant.ts
+++ apps/client/src/app/constants/competition/label.constant.ts
@@ -2,7 +2,7 @@ import { CompetitionCode } from '../../constants';
 import { CompetitionLabel } from '../../models';
 
 export const COMPETITION_LABEL: Record<CompetitionCode, CompetitionLabel> = {
-  [CompetitionCode.ENGLAND_PREMIER_LEAGUE]: 'Premiere League',
+  [CompetitionCode.ENGLAND_PREMIER_LEAGUE]: 'Premier League',
   [CompetitionCode.GERMANY_BUNDESLIGA]: 'Bundesliga',
   [CompetitionCode.GERMANY_BUNDESLIGA_2]: '2. Bundesliga',
   [CompetitionCode.ITALY_SERIE_A]: 'Serie A',
