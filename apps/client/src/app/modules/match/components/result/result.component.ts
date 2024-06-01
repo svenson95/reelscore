bd2d83b17c5a3262a71baa9da0c3bdd6bb15ae6a
Sven Brodny <svenbrodny@googlemail.com>
2024-06-01 20:37:11 +0200

    style(match): date label & some refactoring

--- apps/client/src/app/modules/match/components/result/result.component.ts
+++ apps/client/src/app/modules/match/components/result/result.component.ts
@@ -1,5 +1,10 @@
 import { DatePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  signal,
+} from '@angular/core';
 
 import { MatchDTO } from '@lib/models';
 
@@ -11,60 +16,43 @@ import { OptimizedImageComponent } from '../../../../components';
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [OptimizedImageComponent, DatePipe],
   styles: `
-    :host { @apply flex flex-col; }
-    section { @apply w-full self-center; }
-    section.details { @apply flex items-center; }
-    section.details div { @apply flex flex-1 gap-2 items-center justify-center; }
-    section.details div.team-column { @apply gap-5; }
-    section.details div:not(.team-column) { @apply gap-2; }
-    section.date div { @apply flex w-full gap-3 items-center leading-[20px]; }
-    .team-name { @apply text-fb-font-size-body-1 leading-[18px]; }
-    .date-label { @apply flex-1 text-fb-font-size-body-2; }
+    :host { @apply flex; }
+    div { @apply flex flex-1 gap-2 items-center justify-center text-fb-font-size-body-2 sm:text-fb-font-size-body-1; }
+    .team-column { @apply gap-3; }
+    .result-column { @apply gap-2; }
+    .team-name { @apply leading-[14px]; }
   `,
   template: `
-    <section class="date">
-      <div>
-        <span class="date-label text-right">
-          {{ data().fixture.date | date : 'dd.MM.yyyy' }}
-        </span>
-        <span class="date-label">
-          {{ data().fixture.date | date : 'HH:mm' }}
-        </span>
-      </div>
-    </section>
+    <div class="team-column">
+      <futbet-optimized-image
+        [source]="data().teams.home.logo"
+        alternate="home logo"
+        width="50"
+        height="50"
+      />
+      <span class="team-name">{{ data().teams.home.name }}</span>
+    </div>
 
-    <section class="details">
-      <div class="team-column">
-        <futbet-optimized-image
-          [source]="data().teams.home.logo"
-          alternate="home logo"
-          width="50"
-          height="50"
-        />
-        <span class="team-name">{{ data().teams.home.name }}</span>
-      </div>
+    <div class="result-column">
+      @switch(isUpcoming()) { @case(true) {
+      <span>{{ data().score.fulltime.home }}</span>
+      <span>:</span>
+      <span>{{ data().score.fulltime.away }}</span>
+      } @case(false) { }}
+    </div>
 
-      <div>
-        @switch(isUpcoming()) { @case(true) {
-        <span>{{ data().score.fulltime.home }}</span>
-        <span>:</span>
-        <span>{{ data().score.fulltime.away }}</span>
-        } @case(false) { }}
-      </div>
-
-      <div class="team-column">
-        <span class="team-name text-right">{{ data().teams.away.name }}</span>
-        <futbet-optimized-image
-          [source]="data().teams.away.logo"
-          alternate="away logo"
-          width="50"
-          height="50"
-        />
-      </div>
-    </section>
+    <div class="team-column">
+      <span class="team-name text-right">{{ data().teams.away.name }}</span>
+      <futbet-optimized-image
+        [source]="data().teams.away.logo"
+        alternate="away logo"
+        width="50"
+        height="50"
+      />
+    </div>
   `,
 })
 export class MatchResultComponent {
-  isUpcoming = input.required<boolean>();
   data = input.required<MatchDTO>();
+  isUpcoming = signal<boolean>(true); // TODO derive value from fixture date
 }
