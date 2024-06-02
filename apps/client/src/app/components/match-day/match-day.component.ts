bdff1e2a7994fb004e8a6f83589334d13740888d
Sven Brodny <svenbrodny@googlemail.com>
2024-06-02 19:21:20 +0200

    fix(match-day): empty list label

--- apps/client/src/app/components/match-day/match-day.component.ts
+++ apps/client/src/app/components/match-day/match-day.component.ts
@@ -4,44 +4,48 @@ import {
   computed,
   inject,
 } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { MatchDTO } from '@lib/models';
 
 import { CompetitionFixtures, FilteredCompetitions } from '../../models';
 import { FixturesService, LeagueService } from '../../services';
-
 import { MatchDayListComponent } from './components';
 
 @Component({
   selector: 'futbet-match-day',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [MatchDayListComponent],
+  imports: [MatchDayListComponent, MatProgressSpinnerModule],
   styles: `
+    :host { @apply flex flex-col; }
+
     futbet-match-day-list:not(:last-child) {
         @apply flex flex-col mb-5;
     }
 
     p {
-        @apply text-fb-font-size-body-1 text-fb-color-text-2 text-center py-5;
+        @apply text-fb-font-size-body-1 text-fb-color-text-2 text-center py-10;
     }
   `,
   template: `
-    @for (competition of competitions(); track competition.name) {
+    @if (competitions() === undefined) {
+    <mat-spinner class="my-10 mx-auto" diameter="20" />
+    } @else { @for (competition of competitions(); track competition.name) {
     <futbet-match-day-list [competition]="competition" />
     } @empty {
     <p>Es finden keine Spiele statt.</p>
-    }
+    } }
   `,
 })
 export class MatchDayComponent {
   ls = inject(LeagueService);
   fs = inject(FixturesService);
 
-  competitions = computed<CompetitionFixtures[]>(() => {
+  competitions = computed<CompetitionFixtures[] | undefined>(() => {
     const data = this.fs.fixtures();
     const selectedLeague = this.ls.selectedLeague();
-    if (data === undefined) return [];
+    if (data === undefined) return undefined;
 
     const competitions = [...new Set(data.map((f) => f.league.name))];
     const fixtures = competitions.map((c) => this.filterByCompetition(c, data));
