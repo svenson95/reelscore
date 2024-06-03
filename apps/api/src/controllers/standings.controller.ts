7d9a751361dfd636b77d2df7a37e209209c72462
Sven Brodny <svenbrodny@googlemail.com>
2024-06-03 16:59:31 +0200

    refactor(api): standings routes & controller

--- apps/api/src/controllers/standings.controller.ts
+++ apps/api/src/controllers/standings.controller.ts
@@ -1,42 +1,76 @@
+import { fetchFromRapidApi } from '../middleware';
 import { Standings } from '../models';
 
-export const getStandings = (req, res, next) => {
+export const getStandings = async (req, res, next) => {
   const leagueId = req.query.league;
   const season = 2023;
 
   if (leagueId !== undefined) {
     const query = { 'league.id': leagueId, 'league.season': season };
-    Standings.find(query)
-      .sort({ _id: -1 })
-      .then((data) => next(data[0]))
-      .catch((error) =>
-        res.json({
-          status: 'error happened',
-          error: error,
-        })
-      );
-  } else {
+
+    try {
+      const docs = await Standings.find(query).sort({ _id: -1 });
+      return next(docs[0]);
+    } catch (error) {
+      return res.json({
+        status: 'error happened',
+        error,
+      });
+    }
+  }
+
+  if (leagueId === undefined) {
     const query = {
       'league.id': ['39', '78', '135', '140', '61'],
       $and: [{ 'league.season': season }],
     };
-    Standings.find(query)
-      .sort({ _id: -1 })
-      .limit(5)
-      .then((data) => next(topFiveRanks(data)))
-      .catch((error) =>
-        res.json({
-          status: 'error happened',
-          error: error,
-        })
-      );
+
+    try {
+      const docs = await Standings.find(query).sort({ _id: -1 }).limit(5);
+      const topFiveRanks = (data) =>
+        data.map((d) => ({
+          league: {
+            ...d.league,
+            standings: [d.league.standings[0].slice(0, 5)],
+          },
+        }));
+      return next(topFiveRanks(docs));
+    } catch (error) {
+      return res.json({
+        status: 'error happened',
+        error,
+      });
+    }
   }
 };
 
-const topFiveRanks = (data) =>
-  data.map((d) => ({
-    league: {
-      ...d.league,
-      standings: [d.league.standings[0].slice(0, 5)],
-    },
-  }));
+export const getAllStandings = async (req, res, next) => {
+  try {
+    const docs = await Standings.find();
+    return next(docs);
+  } catch (error) {
+    return res.json({
+      status: 'error happened',
+      error,
+    });
+  }
+};
+
+export const fetchStandings = async (req, res, next) => {
+  const leagueId = req.query.league;
+
+  const uri = `standings?season=2023&league=${leagueId}`;
+  const response = await fetchFromRapidApi(uri);
+  const body = await response.json();
+  const data = body.response[0];
+
+  try {
+    const docs = new Standings(data);
+    return next(docs);
+  } catch (error) {
+    return res.json({
+      status: 'error happened',
+      error,
+    });
+  }
+};
--- apps/api/src/routes/standings.route.ts
+++ apps/api/src/routes/standings.route.ts
@@ -1,49 +1,26 @@
 import express from 'express';
 
-import { getStandings } from '../controllers';
-import { fetchFromRapidApi } from '../middleware';
-import { Standings } from '../models';
+import { fetchStandings, getAllStandings, getStandings } from '../controllers';
 
 export const standings = express.Router();
 
-standings.get('/get', (req, res) =>
-  getStandings(req, res, (data) => {
-    res.json(data);
-  })
-);
+standings.get('/get', async (req, res) => {
+  await getStandings(req, res, (docs) => {
+    return res.json(docs);
+  });
+});
 
-standings.get('/get-all', (req, res) => {
-  Standings.find()
-    .then((docs) => res.json(docs))
-    .catch((error) =>
-      res.json({
-        status: 'error happened',
-        error: error,
-      })
-    );
+standings.get('/get-all', async (req, res) => {
+  await getAllStandings(req, res, (docs) => {
+    return res.json(docs);
+  });
 });
 
 standings.get('/fetch', async (req, res) => {
-  const leagueId = req.query.league;
-
-  const uri = `standings?season=2023&league=${leagueId}`;
-  const response = await fetchFromRapidApi(uri);
-  const body = (await response.json()) as any; // TODO fix any
-  const data = body.response[0];
-
-  const standing = new Standings(data);
-  await standing
-    .save()
-    .then((document) => {
-      res.json({
-        response: 'document saved',
-        document: document,
-      });
-    })
-    .catch((error) => {
-      res.json({
-        response: 'error happened',
-        error: error,
-      });
+  await fetchStandings(req, res, (docs) => {
+    return res.json({
+      response: 'document saved',
+      document: docs,
     });
+  });
 });
