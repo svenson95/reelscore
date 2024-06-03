c84c9aca7d080ffb0d381351b3fbe1cbe089aba4
Sven Brodny <svenbrodny@googlemail.com>
2024-06-03 12:14:33 +0200

    refactor(admin): page component

--- apps/client/src/app/modules/admin/admin.component.ts
+++ apps/client/src/app/modules/admin/admin.component.ts
@@ -1,58 +1,25 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { ContentComponent } from './components/content.component';
+import { SidebarComponent } from './components/sidebar.component';
+
 @Component({
   selector: 'futbet-admin',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
+  imports: [SidebarComponent, ContentComponent],
   styles: `
     :host { @apply w-full flex flex-col gap-5; }
     .page-title { @apply text-center border-b-[1px] pb-2 mb-2; }
     div { @apply flex gap-5; }
-    .sidebar { @apply flex flex-col flex-[1] min-w-[160px] gap-5; }
-    .content { @apply flex-[3]; }
-    section .group-title { @apply text-fb-color-text-2 border-b-[1px] pb-2 mb-2; }
-    .group-title, li { @apply px-3; }
-    li span:last-of-type { @apply float-right; }
   `,
   template: `
     <h3 class="page-title">ADMIN</h3>
 
     <div>
-      <section class="sidebar">
-        <section class="nav">
-          <h4 class="group-title">Datenbank</h4>
-          <ul>
-            <li>Standings</li>
-            <li>Fixtures</li>
-            <li>Fixture-Statistics</li>
-          </ul>
-        </section>
+      <futbet-admin-sidebar />
 
-        <section class="overview">
-          <h4 class="group-title">Übersicht</h4>
-          <ul>
-            <li>
-              <span>Datensätze insgesamt:</span>
-              <span>4</span>
-            </li>
-            <li>
-              <span>Standings Datensätze:</span>
-              <span>2</span>
-            </li>
-            <li>
-              <span>Fixtures Datensätze:</span>
-              <span>1</span>
-            </li>
-            <li>
-              <span>Fixture-Statistics Datensätze:</span>
-              <span>1</span>
-            </li>
-          </ul>
-        </section>
-      </section>
-
-      <section class="content">content ...</section>
+      <futbet-admin-content />
     </div>
   `,
 })
---
+++ apps/client/src/app/modules/admin/components/sidebar.component.ts
@@ -0,0 +1,47 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'futbet-admin-sidebar',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [],
+  styles: `
+    :host { @apply flex flex-col flex-[1] min-w-[160px] gap-10; }
+    .group-title { @apply text-fb-color-text-2 border-b-[1px] pb-2 mb-2; }
+    .group-title, li { @apply px-3 py-1; }
+    li span:last-of-type { @apply float-right; }
+  `,
+  template: `
+    <section class="nav">
+      <h4 class="group-title">Datenbank</h4>
+      <ul>
+        <li>Standings</li>
+        <li>Fixtures</li>
+        <li>Fixture-Statistics</li>
+      </ul>
+    </section>
+
+    <section class="overview">
+      <h4 class="group-title">Übersicht</h4>
+      <ul>
+        <li>
+          <span>Datensätze insgesamt:</span>
+          <span>4</span>
+        </li>
+        <li>
+          <span>Standings Datensätze:</span>
+          <span>2</span>
+        </li>
+        <li>
+          <span>Fixtures Datensätze:</span>
+          <span>1</span>
+        </li>
+        <li>
+          <span>Fixture-Statistics Datensätze:</span>
+          <span>1</span>
+        </li>
+      </ul>
+    </section>
+  `,
+})
+export class SidebarComponent {}
