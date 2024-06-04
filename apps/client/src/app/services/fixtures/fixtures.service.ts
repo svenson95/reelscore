83a6e07da94cd9268b54f0c9007ea9af775182c5
Sven Brodny <svenbrodny@googlemail.com>
2024-06-04 16:26:04 +0200

    fix(match-day): loading state

--- apps/client/src/app/services/fixtures/fixtures.service.ts
+++ apps/client/src/app/services/fixtures/fixtures.service.ts
@@ -1,12 +1,19 @@
-import { Injectable, Signal, inject } from '@angular/core';
+import {
+  Injectable,
+  Signal,
+  WritableSignal,
+  inject,
+  signal,
+} from '@angular/core';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
-import { Observable, switchMap } from 'rxjs';
+import { Observable, switchMap, tap } from 'rxjs';
 
 import { FixtureId, LatestFixturesDTO, MatchDTO } from '@lib/models';
 
 import { DateService, HttpFixturesService } from '../../services';
 export abstract class FixturesService {
   abstract fixtures: Signal<MatchDTO[] | undefined>;
+  abstract isLoading: WritableSignal<boolean>;
   abstract loadLatestFixtures(id: FixtureId): Observable<LatestFixturesDTO>;
   abstract loadFixture(id: FixtureId): Observable<MatchDTO>;
 }
@@ -16,14 +23,18 @@ export class AbstractedFixturesService extends FixturesService {
   http = inject(HttpFixturesService);
   date = inject(DateService);
 
+  isLoading = signal<boolean>(false);
+
   fixtures = toSignal(
     toObservable(this.date.selectedDay).pipe(
+      tap(() => this.isLoading.set(true)),
       switchMap((date) => {
         const d = new Date(date);
         const nextDay = new Date(d.setDate(d.getDate() + 1)).toISOString();
         const dateString = nextDay.split('T')[0];
         return this.http.getFixtures(dateString);
-      })
+      }),
+      tap(() => this.isLoading.set(false))
     )
   );
 
