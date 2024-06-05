e0151f23eebb53d43babf278890fad7702344df4
Sven Brodny <svenbrodny@googlemail.com>
2024-06-05 02:33:00 +0200

    style(statistics): two stats per row

--- apps/client/src/app/modules/match/components/statistics/statistics.component.ts
+++ apps/client/src/app/modules/match/components/statistics/statistics.component.ts
@@ -15,144 +15,154 @@ import { StatisticList } from './models';
   imports: [],
   styles: `
     :host { @apply flex flex-col bg-white border-[1px]; }
-    section { @apply my-5; }
-    div { @apply flex gap-5 justify-center mb-5; }
+    section { @apply flex flex-col my-5 gap-5; }
+    section > div { @apply flex; }
+    div > div { @apply basis-6/12; }
+    div > div > div { @apply flex gap-5 justify-center; }
     h4, li { @apply text-fb-font-size-body-2 md:text-fb-font-size-body-1;}
     h4 { @apply text-fb-color-text-2 text-center mb-2; }
   `,
   template: `
     <h3 class="match-section-title">STATISTIKEN</h3>
     <section>
-      <h4>Ballbesitz</h4>
       <div>
-        <ul>
-          <li>{{ stats().ballPossession.home }}</li>
-        </ul>
+        <div>
+          <h4>Schüsse</h4>
+          <div>
+            <ul>
+              <li>{{ stats().shotsTotal.home }}</li>
+            </ul>
 
-        <ul>
-          <li>{{ stats().ballPossession.away }}</li>
-        </ul>
+            <ul>
+              <li>{{ stats().shotsTotal.away }}</li>
+            </ul>
+          </div>
+        </div>
+
+        <div>
+          <h4>Torschüsse</h4>
+          <div>
+            <ul>
+              <li>{{ stats().shotsOnGoal.home }}</li>
+            </ul>
+
+            <ul>
+              <li>{{ stats().shotsOnGoal.away }}</li>
+            </ul>
+          </div>
+        </div>
       </div>
 
-      <h4>Schüsse insgesamt</h4>
       <div>
-        <ul>
-          <li>{{ stats().shotsTotal.home }}</li>
-        </ul>
+        <div>
+          <h4>Ballbesitz</h4>
+          <div>
+            <ul>
+              <li>{{ stats().ballPossession.home }}</li>
+            </ul>
 
-        <ul>
-          <li>{{ stats().shotsTotal.away }}</li>
-        </ul>
+            <ul>
+              <li>{{ stats().ballPossession.away }}</li>
+            </ul>
+          </div>
+        </div>
+
+        <div>
+          <h4>Eckstöße</h4>
+          <div>
+            <ul>
+              <li>{{ stats().cornerKicks.home }}</li>
+            </ul>
+
+            <ul>
+              <li>{{ stats().cornerKicks.away }}</li>
+            </ul>
+          </div>
+        </div>
       </div>
 
-      <h4>Schüsse</h4>
       <div>
-        <ul>
-          <li>{{ stats().shotsOffGoal.home }}</li>
-        </ul>
+        <div>
+          <h4>Fouls</h4>
+          <div>
+            <ul>
+              <li>{{ stats().fouls.home }}</li>
+            </ul>
 
-        <ul>
-          <li>{{ stats().shotsOffGoal.away }}</li>
-        </ul>
+            <ul>
+              <li>{{ stats().fouls.away }}</li>
+            </ul>
+          </div>
+        </div>
+
+        <div>
+          <h4>Abseits</h4>
+          <div>
+            <ul>
+              <li>{{ stats().offsides.home }}</li>
+            </ul>
+
+            <ul>
+              <li>{{ stats().offsides.away }}</li>
+            </ul>
+          </div>
+        </div>
       </div>
 
-      <h4>Schüsse aufs Tor</h4>
       <div>
-        <ul>
-          <li>{{ stats().shotsOnGoal.home }}</li>
-        </ul>
+        <div>
+          <h4>Pässe insgesamt</h4>
+          <div>
+            <ul>
+              <li>{{ stats().passesTotal.home }}</li>
+            </ul>
 
-        <ul>
-          <li>{{ stats().shotsOnGoal.away }}</li>
-        </ul>
+            <ul>
+              <li>{{ stats().passesTotal.away }}</li>
+            </ul>
+          </div>
+        </div>
+
+        <div>
+          <h4>Pass Qoute</h4>
+          <div>
+            <ul>
+              <li>{{ stats().passAccuracy.home }}</li>
+            </ul>
+
+            <ul>
+              <li>{{ stats().passAccuracy.away }}</li>
+            </ul>
+          </div>
+        </div>
       </div>
 
-      <h4>Eckstöße</h4>
       <div>
-        <ul>
-          <li>{{ stats().cornerKicks.home }}</li>
-        </ul>
+        <div>
+          <h4>Gelbe Karten</h4>
+          <div>
+            <ul>
+              <li>{{ stats().yellowCards.home }}</li>
+            </ul>
 
-        <ul>
-          <li>{{ stats().cornerKicks.away }}</li>
-        </ul>
-      </div>
+            <ul>
+              <li>{{ stats().yellowCards.away }}</li>
+            </ul>
+          </div>
+        </div>
 
-      <h4>Fouls</h4>
-      <div>
-        <ul>
-          <li>{{ stats().fouls.home }}</li>
-        </ul>
+        <div>
+          <h4>Rote Karten</h4>
+          <div>
+            <ul>
+              <li>{{ stats().redCards.home }}</li>
+            </ul>
 
-        <ul>
-          <li>{{ stats().fouls.away }}</li>
-        </ul>
-      </div>
-
-      <h4>Geblockte Schüsse</h4>
-      <div>
-        <ul>
-          <li>{{ stats().goalkeeperSaves.home }}</li>
-        </ul>
-
-        <ul>
-          <li>{{ stats().goalkeeperSaves.away }}</li>
-        </ul>
-      </div>
-
-      <h4>Abseits</h4>
-      <div>
-        <ul>
-          <li>{{ stats().offsides.home }}</li>
-        </ul>
-
-        <ul>
-          <li>{{ stats().offsides.away }}</li>
-        </ul>
-      </div>
-
-      <h4>Pässe insgesamt</h4>
-      <div>
-        <ul>
-          <li>{{ stats().passesTotal.home }}</li>
-        </ul>
-
-        <ul>
-          <li>{{ stats().passesTotal.away }}</li>
-        </ul>
-      </div>
-
-      <h4>Pass Qoute</h4>
-      <div>
-        <ul>
-          <li>{{ stats().passAccuracy.home }}</li>
-        </ul>
-
-        <ul>
-          <li>{{ stats().passAccuracy.away }}</li>
-        </ul>
-      </div>
-
-      <h4>Gelbe Karten</h4>
-      <div>
-        <ul>
-          <li>{{ stats().yellowCards.home }}</li>
-        </ul>
-
-        <ul>
-          <li>{{ stats().yellowCards.away }}</li>
-        </ul>
-      </div>
-
-      <h4>Rote Karten</h4>
-      <div>
-        <ul>
-          <li>{{ stats().redCards.home }}</li>
-        </ul>
-
-        <ul>
-          <li>{{ stats().redCards.away }}</li>
-        </ul>
+            <ul>
+              <li>{{ stats().redCards.away }}</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </section>
   `,
