e3f1c30907df47510bd4b8c97b118044d1233098
Sven Brodny <svenbrodny@googlemail.com>
2024-06-05 11:57:28 +0200

    style(calender-week-label): as mat stroked button

--- apps/client/src/app/modules/league/components/date-bar/components/calender-week-label.component.ts
+++ apps/client/src/app/modules/league/components/date-bar/components/calender-week-label.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 import { CalenderWeek } from '@app/models';
 
@@ -6,8 +7,9 @@ import { CalenderWeek } from '@app/models';
   selector: 'futbet-calender-week-label',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [MatButtonModule],
   styles: `:host { @apply flex items-center ml-auto gap-3 text-fb-font-size-body-2 text-fb-color-text-2;  }`,
-  template: `KW {{ week() }}`,
+  template: `<button mat-stroked-button disabled>KW {{ week() }}</button>`,
 })
 export class CalenderWeekLabelComponent {
   week = input.required<CalenderWeek>();
