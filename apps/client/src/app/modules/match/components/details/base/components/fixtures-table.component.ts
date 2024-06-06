6cf52df0ef3784ca62b2a94df44b3eb80f171a02
Sven Brodny <svenbrodny@googlemail.com>
2024-06-06 14:38:51 +0200

    style(latest-fixtures): refactor table to div

--- apps/client/src/app/modules/match/components/details/base/components/fixtures-table.component.ts
+++ apps/client/src/app/modules/match/components/details/base/components/fixtures-table.component.ts
@@ -16,61 +16,52 @@ import { FixtureTeam, MatchDTO, MatchTeams } from '@lib/models';
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [RouterModule, DatePipe, TeamNamePipe],
   styles: `
-    :host { @apply flex-1 py-4 px-4; }
-    table { @apply w-full; }
-    tr { @apply cursor-pointer sm:hover:bg-fb-color-green-1-light; }
-    tr:not(:last-of-type) { @apply border-b-[1px]; }
-    td {
-      @apply text-fb-font-size-small p-2;
-
-      &.date { @apply w-[70px] min-w-[70px] border-r-[1px]; }
-      &.team { @apply w-[30%] leading-[13px]; }
-      &.home { @apply text-right; }
-      &.result { @apply text-center w-[60px]; }
-    }
+    :host { @apply flex-1 p-4 text-fb-font-size-small; }
+    a { @apply flex p-2 sm:hover:bg-fb-color-green-1-light; }
+    a:not(:last-of-type) { @apply border-b-[1px]; }
+    .date { @apply w-[60px] border-r-[1px]; }
+    .team { @apply w-[30%] content-center leading-[13px]; }
+    .home { @apply text-right; }
+    .result { @apply text-center w-[60px]; }
     .is-related-team { @apply font-bold; }
-    .is-winner td:not(.date) { @apply bg-fb-win; }
-    .is-loser td:not(.date) { @apply bg-fb-lose; }
+    .is-winner { @apply bg-fb-win; }
+    .is-loser { @apply bg-fb-lose; }
   `,
   template: `
-    <table>
-      @for(match of latestFixtures(); track match.fixture.id; let idx = $index)
-      {
-      <tr
-        [class.is-winner]="isWinner(match.teams)"
-        [class.is-loser]="isLoser(match.teams)"
-      >
-        <a [routerLink]="['..', match.fixture.id]">
-          <td class="date">
-            <span>{{ match.fixture.date | date : 'dd.MM | ccc' }}</span>
-          </td>
+    @for(match of latestFixtures(); track match.fixture.id; let idx = $index) {
+    <a
+      [routerLink]="['..', match.fixture.id]"
+      [class.is-winner]="isWinner(match.teams)"
+      [class.is-loser]="isLoser(match.teams)"
+    >
+      <div class="date">
+        <span>{{ match.fixture.date | date : 'dd.MM | ccc' }}</span>
+      </div>
 
-          <td class="team home">
-            <span [class.is-related-team]="relatedTeamId(match.teams.home.id)">
-              {{ match.teams.home.name | teamName : 'short' }}
-            </span>
-          </td>
+      <div class="team home">
+        <span [class.is-related-team]="relatedTeamId(match.teams.home.id)">
+          {{ match.teams.home.name | teamName : 'short' }}
+        </span>
+      </div>
 
-          <td class="result">
-            @if(match.score.fulltime.home !== null) {
-            <span>
-              {{ match.score.fulltime.home }} -
-              {{ match.score.fulltime.away }}
-            </span>
-            }
-          </td>
+      <div class="result">
+        @if(match.score.fulltime.home !== null) {
+        <span>
+          {{ match.score.fulltime.home }} -
+          {{ match.score.fulltime.away }}
+        </span>
+        }
+      </div>
 
-          <td class="team">
-            <span [class.is-related-team]="relatedTeamId(match.teams.away.id)">
-              {{ match.teams.away.name | teamName : 'short' }}
-            </span>
-          </td>
-        </a>
-      </tr>
-      } @empty {
-      <p class="no-data">Keine Spiele gefunden.</p>
-      }
-    </table>
+      <div class="team">
+        <span [class.is-related-team]="relatedTeamId(match.teams.away.id)">
+          {{ match.teams.away.name | teamName : 'short' }}
+        </span>
+      </div>
+    </a>
+    } @empty {
+    <p class="no-data">Keine Spiele gefunden.</p>
+    }
   `,
 })
 export class MatchFixturesTableComponent {
