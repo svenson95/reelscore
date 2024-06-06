61d5b545ba4b8cb8331a929e82a18570fabde526
Sven Brodny <svenbrodny@googlemail.com>
2024-06-06 15:22:06 +0200

    refactor(rapid): service return types

--- apps/client/src/app/modules/admin/services/rapid.service.ts
+++ apps/client/src/app/modules/admin/services/rapid.service.ts
@@ -2,16 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 
-import { FixtureId, FixtureStatisticsDTO } from '@lib/models';
+import {
+  FixtureId,
+  FixtureStatisticsDTO,
+  MatchDTO,
+  StandingsDTO,
+} from '@lib/models';
 import { environment } from '../../../../environments/environment';
 import { CompetitionId } from '../../../models';
 
 export abstract class HttpRapidService {
-  abstract fetchStandings(leagueId: CompetitionId): Observable<unknown>;
+  abstract fetchStandings(leagueId: CompetitionId): Observable<StandingsDTO>;
   abstract fetchFixtures(
     leagueId: CompetitionId,
     matchDay: number
-  ): Observable<unknown>;
+  ): Observable<MatchDTO>;
   abstract fetchStatistics(
     fixtureId: FixtureId
   ): Observable<FixtureStatisticsDTO>;
@@ -23,18 +28,18 @@ export class AbstractedHttpRapidService extends HttpRapidService {
 
   http = inject(HttpClient);
 
-  fetchStandings(leagueId: CompetitionId): Observable<unknown> {
+  fetchStandings(leagueId: CompetitionId): Observable<StandingsDTO> {
     const url = this.BASE_URL + `standings/fetch?league=${leagueId}`;
-    return this.http.get(url);
+    return this.http.get<StandingsDTO>(url);
   }
 
   fetchFixtures(
     leagueId: CompetitionId,
     matchDay: number
-  ): Observable<unknown> {
+  ): Observable<MatchDTO> {
     const url =
       this.BASE_URL + `fixtures/fetch?league=${leagueId}&round=${matchDay}`;
-    return this.http.get(url);
+    return this.http.get<MatchDTO>(url);
   }
 
   fetchStatistics(fixtureId: FixtureId): Observable<FixtureStatisticsDTO> {
