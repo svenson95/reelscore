bb8002a6023be5a7e6d31a4cc3850ae4d02e2fe9
Sven Brodny <svenbrodny@googlemail.com>
2024-06-08 16:46:56 +0200

    fix(api): get events & statistics

--- apps/api/src/models/fixture-events.ts
+++ apps/api/src/models/fixture-events.ts
@@ -1,14 +1,60 @@
 import { FixtureEventsDTO } from '@lib/models';
 import mongoose from 'mongoose';
 
-const FixtureEventsSchema = new mongoose.Schema<FixtureEventsDTO>({
+const EventsSchema = new mongoose.Schema<FixtureEventsDTO>({
   parameters: {
-    fixture: String,
+    fixture: Number,
   },
-  response: [],
+  response: [
+    {
+      time: {
+        elapsed: Number,
+        extra: Number,
+      },
+      team: {
+        id: Number,
+        name: String,
+        logo: String,
+      },
+      player: {
+        id: Number,
+        name: String,
+      },
+      assist: {
+        id: Number,
+        name: String,
+      },
+      type: {
+        type: String,
+        enum: ['Goal', 'Card', 'Subst', 'Var'],
+      },
+      detail: {
+        type: String,
+        enum: [
+          'Normal Goal',
+          'Own Goal',
+          'Penalty',
+          'Missed Penalty',
+          'Yellow Card',
+          'Red Card',
+          'Goal cancelled',
+          'Penalty confirmed',
+          'Substitution',
+          'Substitution [1]',
+          'Substitution [2]',
+          'Substitution [3]',
+          'Substitution [4]',
+          'Substitution [5]',
+          'Substitution [6]',
+          'Substitution [7]',
+          'Substitution [8]',
+          'Substitution [9]',
+          'Substitution [10]',
+        ],
+      },
+      comments: String,
+    },
+  ],
 });
 
-export const FixtureEvents = mongoose.model(
-  'fixture-events',
-  FixtureEventsSchema
-);
+export const FixtureEvents = mongoose.model('fixture-events', EventsSchema);
