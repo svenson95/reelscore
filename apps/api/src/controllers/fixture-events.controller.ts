a83de929c3704cc528d4ad538e2a37ee09bd1ec7
Sven Brodny <svenbrodny@googlemail.com>
2024-06-09 23:26:52 +0200

    fix(fixture-events): sorting in backend

--- apps/api/src/controllers/fixture-events.controller.ts
+++ apps/api/src/controllers/fixture-events.controller.ts
@@ -1,3 +1,4 @@
+import { FixtureEventsResponse } from '@lib/models';
 import { FixtureEvents } from '../models';
 
 export const getFixtureEventsById = async (req, res, next) => {
@@ -6,11 +7,21 @@ export const getFixtureEventsById = async (req, res, next) => {
     const docs = await FixtureEvents.find().where('parameters').equals({
       fixture,
     });
-    return next(docs[0] ?? null);
+
+    if (docs.length === 0) {
+      return null;
+    }
+    const sortedEvents = sortEvents(docs[0].response);
+    return next({ ...docs[0], response: sortedEvents });
   } catch (error) {
     return res.json({
       status: 'error happened',
       error,
     });
   }
+};
+
+const sortEvents = (d: FixtureEventsResponse[]) => {
+  const time = (t: FixtureEventsResponse) => t.time.elapsed + t.time.extra;
+  return d.sort((a, b) => time(b) - time(a));
 };
