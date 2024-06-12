d1378ad70b5c7b2fc2650839f76453af1f7e5d89
Sven Brodny <svenbrodny@googlemail.com>
2024-06-12 15:32:46 +0200

    style(match): gap between components

--- apps/client/src/app/modules/match/components/details/base/base.component.ts
+++ apps/client/src/app/modules/match/components/details/base/base.component.ts
@@ -12,7 +12,7 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [MatchFixtureDataComponent, MatchLatestFixturesComponent],
   styles: `
-    :host { @apply flex flex-col gap-10; }
+    :host { @apply flex flex-col gap-5; }
     .item { @apply flex justify-center py-2 gap-5; }
     .item > *:not(.key) { @apply flex-[2] sm:flex-1; }
     .key { @apply text-fb-color-text-2 text-right flex-1; }
