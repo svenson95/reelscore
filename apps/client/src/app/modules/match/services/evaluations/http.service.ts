b1ad479926528d152739a2a31deba01b986b03e6
Sven Brodny <svenbrodny@googlemail.com>
2024-06-17 16:09:56 +0200

    feat(client): fixture performances

---
+++ apps/client/src/app/modules/match/services/evaluations/http.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { EvaluationDTO, FixtureId } from '@lib/models';
+import { environment } from '../../../../../environments/environment';
+
+export abstract class HttpEvaluationsService {
+  abstract getEvaluations(fixtureId: FixtureId): Observable<EvaluationDTO>;
+}
+
+@Injectable()
+export class AbstractedHttpEvaluationsService extends HttpEvaluationsService {
+  BASE_URL = environment.api + 'fixture-evaluations';
+
+  http = inject(HttpClient);
+
+  getEvaluations(fixtureId: FixtureId): Observable<EvaluationDTO> {
+    const params = new HttpParams().set('fixture', fixtureId);
+    return this.http.get<EvaluationDTO>(this.BASE_URL + '/get', {
+      params,
+    });
+  }
+}
+
+export const HTTP_EVALUATIONS_SERVICE_PROVIDER = {
+  provide: HttpEvaluationsService,
+  useClass: AbstractedHttpEvaluationsService,
+};
