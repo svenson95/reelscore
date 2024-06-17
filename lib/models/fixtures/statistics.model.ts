e143c2797e842300b27bacc8838203c3b8ce8f2f
Sven Brodny <svenbrodny@googlemail.com>
2024-06-17 16:10:10 +0200

    refactor(lib): statistic item model

--- lib/models/fixtures/statistics.model.ts
+++ lib/models/fixtures/statistics.model.ts
@@ -1,6 +1,10 @@
 export type TeamDetails = { id: number; name: string; logo: string };
+export type StatisticItemType = string;
 export type StatisticItemValue = string | number | null;
-export type StatisticItem = { type: string; value: StatisticItemValue };
+export type StatisticItem = {
+  type: StatisticItemType;
+  value: StatisticItemValue;
+};
 export type TeamStatistics = Array<StatisticItem>;
 export interface StatisticDTO {
   team: TeamDetails;
