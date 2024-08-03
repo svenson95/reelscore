4074cc3ecc8c15afbc6755b5963d9830a620bf81
Sven Brodny <svenbrodny@googlemail.com>
2024-08-03 12:25:31 +0200

    style(footer): increase y padding

--- apps/client/src/app/components/footer/footer.component.ts
+++ apps/client/src/app/components/footer/footer.component.ts
@@ -8,7 +8,7 @@ import { LogoComponent } from '../logo/logo.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [LogoComponent],
   styles: `
-    :host { @apply flex justify-center px-fb-padding-3 py-10 border-t-[1px]; }
+    :host { @apply flex justify-center px-fb-padding-3 py-20 border-t-[1px]; }
     reelscore-logo { @apply grayscale opacity-35; }
   `,
   template: `
