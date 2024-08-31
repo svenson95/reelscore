faedaf7e3e7f3dbad4362da624d7c61ac28f72e3
Sven Brodny <svenbrodny@googlemail.com>
2024-08-31 15:24:51 +0200

    test: trigger deployment

--- apps/api/src/routes/evaluations.route.ts
+++ apps/api/src/routes/evaluations.route.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { getFixtureEvaluations } from '../controllers';
 
 export const fixtureEvaluations = express.Router();
+//
 
 fixtureEvaluations.get('/get', async (req, res) => {
   await getFixtureEvaluations(req, res, (docs) => {
