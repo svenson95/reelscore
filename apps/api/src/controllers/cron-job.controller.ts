78891d6303cb30d5139364323f005da8b3963ff5
Sven Brodny <svenbrodny@googlemail.com>
2024-08-31 15:42:37 +0200

    test: trigger deployment (11)

--- apps/api/src/controllers/cron-job.controller.ts
+++ apps/api/src/controllers/cron-job.controller.ts
@@ -1,7 +1,9 @@
 import { CronLogs } from '../models';
 
-export const cron = async () => {
+export async function GET(request: Request) {
   const log = await CronLogs.findOne().sort({ _id: -1 }).lean();
   const requestCounter = log === null ? 1 : log.requestCounter + 1;
   await CronLogs.create({ requestCounter, date: Date.now() });
-};
+
+  return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+}
