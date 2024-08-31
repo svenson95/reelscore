b8c32fcf2ce5c3ae9c4a3330a929b0d80853d90a
Sven Brodny <svenbrodny@googlemail.com>
2024-08-31 15:41:11 +0200

    test: trigger deployment (10)

--- apps/api/src/controllers/cron-job.controller.ts
+++ apps/api/src/controllers/cron-job.controller.ts
@@ -2,6 +2,6 @@ import { CronLogs } from '../models';
 
 export const cron = async () => {
   const log = await CronLogs.findOne().sort({ _id: -1 }).lean();
-  const requestCounter = log.requestCounter + 1;
+  const requestCounter = log === null ? 1 : log.requestCounter + 1;
   await CronLogs.create({ requestCounter, date: Date.now() });
 };
