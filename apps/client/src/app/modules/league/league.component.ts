6f290b4b98e7b71778fc3a18af5c1de562770d04
Sven Brodny <svenbrodny@googlemail.com>
2024-09-02 23:42:52 +0200

    feat(league): draft without content

--- apps/client/src/app/modules/league/league.component.ts
+++ apps/client/src/app/modules/league/league.component.ts
@@ -1,16 +1,50 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
 
-import { ROUTE_SERVICE_PROVIDER } from '../../services';
+import { getCompetitionLogo } from '@app/models';
+import { ROUTE_SERVICE_PROVIDER } from '@app/services';
+import { OptimizedImageComponent } from '../../components';
 import { RouterView } from '../router-view';
-import { StandingsComponent } from './components';
-import { SERVICE_PROVIDERS } from './services';
 
 @Component({
   selector: 'reelscore-league',
   standalone: true,
-  imports: [StandingsComponent],
-  providers: [...SERVICE_PROVIDERS, ROUTE_SERVICE_PROVIDER],
-  styles: ``,
-  template: ` league works ... `,
+  imports: [MatTabsModule, OptimizedImageComponent],
+  providers: [ROUTE_SERVICE_PROVIDER],
+  styles: `
+    :host { @apply w-full; }
+    .header { @apply w-fit bg-white rounded-full p-8 mx-auto mb-5; }
+    :host ::ng-deep .mat-mdc-tab-body.mat-mdc-tab-body-active { 
+      @apply bg-white; 
+
+      .mat-mdc-tab-body-content { @apply p-5 text-center; }
+    }
+  `,
+  template: `
+    <section class="header">
+      @if (competition()?.id) {
+      <reelscore-optimized-image
+        [source]="getCompetitionLogo(competition()!.id)"
+        [alternate]="competition()!.label"
+        width="64"
+        height="64"
+      />
+      }
+    </section>
+    <section class="league-content">
+      <mat-tab-group>
+        <mat-tab label="Ergebnisse"> Letzte Spiele </mat-tab>
+        <mat-tab label="Spielplan"> Anstehende Spiele </mat-tab>
+        <mat-tab label="Tabellen">
+          Tabelle, Heimtabelle & Auswärtstabelle
+        </mat-tab>
+        <mat-tab label="Statistiken"> Torschützen & Vorlagen Tabellen </mat-tab>
+      </mat-tab-group>
+    </section>
+  `,
 })
-export class LeagueComponent extends RouterView {}
+export class LeagueComponent extends RouterView {
+  competition = this.leagueService.selectedLeague;
+
+  getCompetitionLogo = getCompetitionLogo;
+}
