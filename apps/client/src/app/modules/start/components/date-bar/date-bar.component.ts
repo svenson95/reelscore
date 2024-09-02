21a13afe98fcd5359ad3eefe055b853c8a5f458a
Sven Brodny <svenbrodny@googlemail.com>
2024-09-02 18:26:19 +0200

    feat(search-button): without logic

--- apps/client/src/app/modules/start/components/date-bar/date-bar.component.ts
+++ apps/client/src/app/modules/start/components/date-bar/date-bar.component.ts
@@ -5,6 +5,8 @@ import {
   inject,
 } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { Store } from '@ngrx/store';
 
@@ -25,57 +27,65 @@ import {
     TodayButtonComponent,
     WeekToggleGroupComponent,
     MatProgressSpinnerModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   styles: `
     :host { 
-      @apply flex flex-wrap items-center justify-between mb-5 gap-3 sm:gap-5; 
+      @apply w-full flex flex-wrap items-center justify-between mb-5 gap-5; 
     }
-
-    section { 
-      @apply inline-flex flex-wrap items-center justify-center sm:w-fit gap-5; 
-    }
-      
-    div { @apply flex gap-5 items-center; }
-    .top { @apply w-full xs:w-fit justify-between; }
     mat-spinner { --mdc-circular-progress-active-indicator-color: var(--fb-color-white); }
+    
+    .top { @apply flex w-full sm:w-fit items-center gap-5; }
+    .spacer { @apply flex-grow; }
+    .search-button { 
+      --mdc-icon-button-icon-size: 16px;
+      --mdc-icon-button-state-layer-size: 36px;
+      background-color: var(--fb-color-white); 
+      padding: 6px;
+    }
   `,
   template: `
-    <section>
-      <div class="top">
-        <div>
-          <reelscore-date-picker
-            [selectedDay]="selectedDay()"
-            (dateSelected)="selectedDay.set($event)"
-          />
-          @if (isMobile() && isLoading()) {
-          <mat-spinner diameter="20"></mat-spinner>}
-        </div>
-
-        @if (isMobile()) {
-        <div class="week-and-time">
-          <reelscore-today-button
-            [isToday]="isToday()"
-            (onClick)="selectedDay.set($event)"
-          />
-        </div>
-        }
-      </div>
-
-      <reelscore-week-toogle-group
-        [weekdays]="weekdays()"
+    <div class="top">
+      <reelscore-date-picker
         [selectedDay]="selectedDay()"
         (dateSelected)="selectedDay.set($event)"
       />
-
-      @if (!isMobile() && isLoading()) {
+      @if (isMobile()){
+      <div class="week-and-time">
+        <reelscore-today-button
+          [isToday]="isToday()"
+          (onClick)="selectedDay.set($event)"
+        />
+      </div>
+      @if (isLoading()) {
       <mat-spinner diameter="20"></mat-spinner>}
-    </section>
+      <div class="spacer"></div>
+      <button class="search-button" mat-icon-button aria-label="Search button">
+        <mat-icon>search</mat-icon>
+      </button>
+      }
+    </div>
 
-    @if (!isMobile()) {
-    <reelscore-today-button
-      [isToday]="isToday()"
-      (onClick)="selectedDay.set($event)"
+    <reelscore-week-toogle-group
+      [weekdays]="weekdays()"
+      [selectedDay]="selectedDay()"
+      (dateSelected)="selectedDay.set($event)"
     />
+
+    @if (!isMobile()){
+    <div class="week-and-time">
+      <reelscore-today-button
+        [isToday]="isToday()"
+        (onClick)="selectedDay.set($event)"
+      />
+    </div>
+    @if (isLoading()) {
+    <mat-spinner diameter="20"></mat-spinner>}
+    <div class="spacer"></div>
+    <button class="search-button" mat-icon-button aria-label="Search button">
+      <mat-icon>search</mat-icon>
+    </button>
     }
   `,
 })
