25520bf09f0bed39cfcfa8b0fecc0281cbf672eb
Sven Brodny <svenbrodny@googlemail.com>
2024-09-03 20:27:58 +0200

    feat(league): filter button (1/2)

---
+++ apps/client/src/app/modules/start/components/date-bar/components/action-buttons/action-buttons.component.ts
@@ -0,0 +1,41 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+import { FilterButtonComponent } from './filter-button.component';
+
+@Component({
+  selector: 'reelscore-action-buttons',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+    FilterButtonComponent,
+  ],
+  styles: `
+    :host { 
+      @apply flex gap-5;
+
+      ::ng-deep .mat-mdc-icon-button { 
+        --mdc-icon-button-state-layer-size: 36px;
+        @apply bg-fb-color-white p-[2px] rounded-[0];
+        
+        .mat-icon { @apply w-[20px] h-[20px] text-[20px]; }
+
+        .mat-mdc-button-persistent-ripple { 
+          @apply rounded-[0];
+        }
+      }
+    }
+  `,
+  template: `
+    <reelscore-filter-button />
+    <button mat-icon-button aria-label="Search button" matTooltip="Suche">
+      <mat-icon>search</mat-icon>
+    </button>
+  `,
+})
+export class ActionButtonsComponent {}
---
+++ apps/client/src/app/modules/start/components/date-bar/components/action-buttons/filter-button.component.ts
@@ -0,0 +1,89 @@
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+import { OptimizedImageComponent } from '@app/components';
+import { SELECT_COMPETITION_DATA } from '@app/constants';
+
+import { CompetitionId } from '@lib/models';
+
+@Component({
+  selector: 'reelscore-filter-button',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatMenuModule,
+    OptimizedImageComponent,
+  ],
+  styles: `
+    button[mat-menu-item] { 
+        --mat-menu-item-label-text-size: var(--fb-font-size-small);
+        --mat-menu-item-label-text-line-height: var(--fb-font-size-small);
+        --mat-menu-item-icon-color: var(--fb-color-white-2);
+        @apply min-h-[32px];
+    }
+    button.mdc-icon-button, button.mat-mdc-menu-item {
+        &.is-filtering { @apply bg-blue-500 text-fb-color-white; }
+    }
+    ::ng-deep .filter-menu .mat-mdc-menu-content .mat-mdc-menu-item .mat-mdc-menu-item-text {
+        @apply flex gap-2;
+    }
+    .mat-mdc-menu-item {
+        .mat-icon { --mat-menu-item-icon-size: 12px; font-size: 12px; margin-right: 0; }
+        .reset-button { @apply ml-auto; }
+    }
+  `,
+  template: `
+    <button
+      mat-icon-button
+      aria-label="Filter button"
+      matTooltip="Filtern"
+      [class.is-filtering]="!!selectedCompetition()"
+      [matMenuTriggerFor]="menu"
+    >
+      <mat-icon>filter_list</mat-icon>
+    </button>
+    <mat-menu #menu="matMenu" class="filter-menu">
+      @for (group of groups; track group.label) {
+      <button mat-menu-item class="group-title" disabled>
+        {{ group.label }}
+      </button>
+      @for (competition of group.competitions; track competition.id) {
+      <button
+        mat-menu-item
+        (click)="setFilter(competition.id)"
+        [class.is-filtering]="selectedCompetition() === competition.id"
+      >
+        <reelscore-optimized-image
+          [source]="competition.image"
+          [alternate]="competition.label"
+          width="14"
+          height="14"
+        />
+        <span>{{ competition.label }}</span>
+        <button class="reset-button">
+          <mat-icon>close</mat-icon>
+        </button>
+      </button>
+      } }
+    </mat-menu>
+  `,
+})
+export class FilterButtonComponent {
+  selectedCompetition = signal<CompetitionId | null>(null);
+
+  groups = SELECT_COMPETITION_DATA;
+
+  setFilter(competitionId: CompetitionId) {
+    if (this.selectedCompetition() === competitionId) {
+      this.selectedCompetition.set(null);
+    } else {
+      this.selectedCompetition.set(competitionId);
+    }
+  }
+}
