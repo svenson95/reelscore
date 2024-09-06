32aa38885b285601971100e71923ebaec92dbeb0
Sven Brodny <svenbrodny@googlemail.com>
2024-09-06 20:59:04 +0200

    data(team): name pipe new type 'abbreviation'

--- apps/client/src/app/pipes/team/team-name.pipe.ts
+++ apps/client/src/app/pipes/team/team-name.pipe.ts
@@ -1,8 +1,9 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { abbreviationTeamName } from './abbreviation-names.data';
 import { longTeamName } from './long-names.data';
 import { shortTeamName } from './short-names.data';
 
-type TeamNameOption = 'short' | 'long';
+type TeamNameOption = 'abbreviation' | 'short' | 'long';
 
 @Pipe({
   name: 'teamName',
@@ -10,6 +11,13 @@ type TeamNameOption = 'short' | 'long';
 })
 export class TeamNamePipe implements PipeTransform {
   transform(value: string, options: TeamNameOption = 'long'): string {
-    return options === 'short' ? shortTeamName(value) : longTeamName(value);
+    switch (options) {
+      case 'abbreviation':
+        return abbreviationTeamName(value);
+      case 'short':
+        return shortTeamName(value);
+      case 'long':
+        return longTeamName(value);
+    }
   }
 }
