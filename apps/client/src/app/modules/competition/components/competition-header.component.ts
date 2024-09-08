0d9a1f0bb76c48b99ef89b2145075efdbf42cd0d
Sven Brodny <svenbrodny@googlemail.com>
2024-09-08 15:11:57 +0200

    style(competition): fix back-button z-index

--- apps/client/src/app/modules/competition/components/competition-header.component.ts
+++ apps/client/src/app/modules/competition/components/competition-header.component.ts
@@ -10,7 +10,7 @@ import { LeagueService } from '@app/services';
   imports: [OptimizedImageComponent, BackButtonComponent],
   styles: `
     :host { @apply relative; }
-    reelscore-back-button { @apply absolute top-0 left-0; }
+    reelscore-back-button { @apply absolute top-0 left-0 z-10; }
     reelscore-optimized-image { @apply w-fit bg-white rounded-full p-8 mx-auto mb-5; }
   `,
   template: `
