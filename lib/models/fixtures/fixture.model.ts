0ceb8e77a05668428710791e208c0b2b63a9160f
Sven Brodny <svenbrodny@googlemail.com>
2024-09-10 20:11:47 +0200

    build(models): add match statuses type & finished match statuses

--- lib/models/fixtures/fixture.model.ts
+++ lib/models/fixtures/fixture.model.ts
@@ -4,10 +4,61 @@ import { Team } from '../team.model';
 import { FixturePrediction } from './evaluated-fixture.model';
 import { EventDTO } from './events.model';
 
+export type StatusLong =
+  | 'Time To Be Defined'
+  | 'Not Started'
+  | 'First Half, Kick Off'
+  | 'Halftime'
+  | 'Second Half, 2nd Half Started'
+  | 'Extra Time'
+  | 'Break Time'
+  | 'Penalty in Progress'
+  | 'Match Suspended'
+  | 'Match Interrupted'
+  | 'Match Finished'
+  | 'Match Postponed'
+  | 'Match Cancelled'
+  | 'Match Abandoned'
+  | 'Technical Loss'
+  | 'WalkOver'
+  | 'In Progress';
+export type StatusShort =
+  | 'TBD'
+  | 'NS'
+  | '1H'
+  | 'HT'
+  | '2H'
+  | 'ET'
+  | 'BT'
+  | 'P'
+  | 'SUSP'
+  | 'INT'
+  | 'FT'
+  | 'AET'
+  | 'PEN'
+  | 'PST'
+  | 'CANC'
+  | 'ABD'
+  | 'AWD'
+  | 'WO'
+  | 'LIVE';
+export type FixtureDetail =
+  | 'lineups'
+  | 'fixture-statistics'
+  | 'players-statistics'
+  | 'events';
+
+export const finishedMatchStatuses = ['FT', 'AET', 'PEN'] as const;
+export type FinishedMatchStatus = (typeof finishedMatchStatuses)[number];
+
 export type FixtureId = number | string; // TODO: refactor to string only
 export type FixturePeriods = { first: number; second: number };
 export type FixtureVenue = { id: number | null; name: string; city: string };
-export type FixtureStatus = { long: string; short: string; elapsed: number };
+export type FixtureStatus = {
+  long: StatusLong;
+  short: StatusShort;
+  elapsed: number;
+};
 export type FixtureResult = 'NO_RESULT_AVAILABLE' | 'WIN' | 'DRAW' | 'LOSS';
 export interface Fixture {
   id: FixtureId;
