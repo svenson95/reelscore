f9df11791e5abe792d7559d6bf097844199ed728
Sven Brodny <svenbrodny@googlemail.com>
2024-09-15 00:37:14 +0200

    fix(match): hide fixture-standings if isKoPhase

--- lib/shared/competition.helper.ts
+++ lib/shared/competition.helper.ts
@@ -1,7 +1,13 @@
-import { CompetitionId } from '../models/competition.model';
+import {
+  CompetitionId,
+  CompetitionRoundString,
+} from '../models/competition.model';
 
 export const isCompetitionWithMultipleGroups = (competitionId: CompetitionId) =>
   [4, 5].includes(competitionId);
 
 export const isCompetitionWithoutStandings = (competitionId: CompetitionId) =>
   [81, 137, 528, 529, 531].includes(competitionId);
+
+export const isKoPhase = (round: CompetitionRoundString) =>
+  ['Round of 16', 'Quarter-finals', 'Final', 'Semi-finals'].includes(round);
