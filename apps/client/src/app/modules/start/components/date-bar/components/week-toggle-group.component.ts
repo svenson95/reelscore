8bbf532c471b47badf0b648c8d86c7b95d6212e8
Sven Brodny <svenbrodny@googlemail.com>
2025-03-11 06:23:42 +0100

    style(app): update button styling & add mat-icon for date-picker * and add new button bg-color & text-color variables

--- apps/client/src/app/modules/start/components/date-bar/components/week-toggle-group.component.ts
+++ apps/client/src/app/modules/start/components/date-bar/components/week-toggle-group.component.ts
@@ -20,7 +20,7 @@ import { DateString, TODAY_ISO_STRING, toIsoString } from '@app/shared';
     :host { @apply w-full xs:w-fit; touch-action: pan-x pan-y; }
     :host mat-button-toggle-group {
       --mat-standard-button-toggle-selected-state-text-color: white; // TODO refactor white var to -1 and -2
-      --mat-standard-button-toggle-selected-state-background-color: var(--fb-color-bg-black);
+      --mat-standard-button-toggle-selected-state-background-color: var(--fb-button-bg-color);
       --mat-standard-button-toggle-height: 34px; 
       @apply flex;
 
