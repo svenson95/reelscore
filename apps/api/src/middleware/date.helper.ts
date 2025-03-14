a13c620cea762aaa6f7de585e971d0492345b23c
Sven Brodny <svenbrodny@googlemail.com>
2025-03-14 18:37:23 +0100

    fix(fixtures): get by-date returns wrong dates

--- apps/api/src/middleware/date.helper.ts
+++ apps/api/src/middleware/date.helper.ts
@@ -1,6 +1,8 @@
 export const getWeekDatesArray = (date: string): string[] => {
   const startOfWeek = new Date(date);
-  startOfWeek.setDate(startOfWeek.getDate() - startOfWeek.getDay() + 1); // Start of the week (Monday)
+  const dayOfWeek = startOfWeek.getDay();
+  const correctedDayOfWeek = dayOfWeek === 0 ? 7 : dayOfWeek; // Handle Sunday as the last day of the week
+  startOfWeek.setDate(startOfWeek.getDate() - correctedDayOfWeek + 1); // Start of the week (Monday)
   const weekFixtures = Array.from({ length: 7 }, (_, i) => {
     const day = new Date(startOfWeek);
     day.setDate(startOfWeek.getDate() + i);
