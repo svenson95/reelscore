87249914e281e688690b7bba553bdbec49dc13a0
Sven Brodny <svenbrodny@googlemail.com>
2025-03-16 13:48:15 +0100

    feat(fixture-list): is-playing styling & some refac

--- apps/client/src/app/shared/pipes/index.ts
+++ apps/client/src/app/shared/pipes/index.ts
@@ -1,3 +1,4 @@
 export * from './competition/competition-label.pipe';
 export * from './competition/competition-round.pipe';
+export * from './is-status/is-status.pipe';
 export * from './team/team-name.pipe';
