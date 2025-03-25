2c579bf5f6356d91c6c45caa97b51ef3308d8628
Sven Brodny <svenbrodny@googlemail.com>
2025-03-25 15:27:16 +0100

    fix(standings): top-five response

--- apps/api/src/services/standings.service.ts
+++ apps/api/src/services/standings.service.ts
@@ -10,19 +10,4 @@ export class StandingsService {
     const standings = await Standings.findOne(filter).sort(sortOptions).lean();
     return standings;
   }
-
-  async findManyByFilter({
-    filter,
-    limit,
-  }: {
-    filter: FilterQuery<unknown>;
-    limit: number;
-  }): Promise<StandingsDTO[]> {
-    const sortOptions: { [key: string]: SortOrder } = { _id: -1 };
-    const standings = await Standings.find(filter)
-      .sort(sortOptions)
-      .limit(limit)
-      .lean();
-    return standings;
-  }
 }
