f208b39599bf382ccec0bffb52ac525ade5936cd
Sven Brodny <svenbrodny@googlemail.com>
2025-03-29 09:31:13 +0100

    style: update some text and border-color to orange * and disable eslint warning for header and footer selector

--- apps/client/src/app/shared/components/footer/footer.component.ts
+++ apps/client/src/app/shared/components/footer/footer.component.ts
@@ -3,12 +3,13 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { LogoComponent } from '../logo/logo.component';
 
 @Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'footer',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [LogoComponent],
   styles: `
-    :host { @apply flex justify-center px-rs-padding-3 py-20 border-t-[1px]; }
+    :host { @apply flex justify-center px-rs-padding-3 py-20 border-t-[1px] border-rs-color-orange; }
   `,
   template: `
     <section>
