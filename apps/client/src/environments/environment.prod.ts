73a0aa0fe9bc6ceb60e7d5f4b5bb16b09710173e
Sven Brodny <svenbrodny@googlemail.com>
2025-03-31 15:48:07 +0200

    test: deploy

--- apps/client/src/environments/environment.prod.ts
+++ apps/client/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   api: 'https://reelscore-api-svenson95s-projects.vercel.app/',
+  websocketUrl: 'wss://reelscore-api-svenson95s-projects.vercel.app/ws',
   isAdmin: false,
 };
