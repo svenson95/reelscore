60016228aa79b964b4b5c12389de6aed5a1e85bd
Sven Brodny <svenbrodny@googlemail.com>
2025-03-31 15:50:20 +0200

    build: optimized prod config (2)

--- apps/client/src/environments/environment.prod.ts
+++ apps/client/src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
   api: 'https://reelscore-api-svenson95s-projects.vercel.app/',
-  websocketUrl: 'wss://reelscore-api-svenson95s-projects.vercel.app/ws',
   isAdmin: false,
 };
