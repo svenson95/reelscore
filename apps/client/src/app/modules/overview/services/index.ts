d600b8c6d95bf6f4c1f132db9584f2ae6f3f206d
Sven Brodny <svenbrodny@googlemail.com>
2025-04-17 22:41:49 +0200

    refactor: date-service in date and selected-date services

--- apps/client/src/app/modules/overview/services/index.ts
+++ apps/client/src/app/modules/overview/services/index.ts
@@ -1,13 +1,16 @@
 import { DATE_SERVICE_PROVIDER } from './date.service';
 import { FILTER_SERVICE_PROVIDER } from './filter.service';
+import { SELECTED_DATE_SERVICE_PROVIDER } from './selected-date.service';
 import { VISIBILITY_OBSERVER_SERVICE_PROVIDER } from './visibility-observer.service';
 
 export * from './date.service';
 export * from './filter.service';
+export * from './selected-date.service';
 export * from './visibility-observer.service';
 
 export const SERVICE_PROVIDERS = [
   DATE_SERVICE_PROVIDER,
+  SELECTED_DATE_SERVICE_PROVIDER,
   FILTER_SERVICE_PROVIDER,
   VISIBILITY_OBSERVER_SERVICE_PROVIDER,
 ];
---
+++ apps/client/src/app/modules/overview/services/selected-date.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, Signal, signal } from '@angular/core';
+import moment from 'moment';
+
+import { DateString } from '../../../shared';
+
+export abstract class SelectedDateService {
+  abstract selectedDay: Signal<DateString>;
+  abstract setSelectedDay(day: DateString): void;
+}
+
+@Injectable()
+export class AbstractedSelectedDateService extends SelectedDateService {
+  private selectedDaySignal = signal<DateString>(this.initialDate);
+  selectedDay = this.selectedDaySignal.asReadonly();
+  setSelectedDay(day: DateString): void {
+    this.selectedDaySignal.set(day);
+  }
+
+  private get initialDate(): DateString {
+    const url = window.location.pathname;
+    return this.extractDateFromUrl(url);
+  }
+
+  private extractDateFromUrl(url: string): DateString {
+    const dateString = url.split('/')[1];
+    const isValidDate = /^\d{4}-\d{2}-\d{2}$/.test(dateString);
+    if (!isValidDate) {
+      console.warn(`Invalid or missing date in URL: ${dateString}`);
+      return moment().tz('Europe/Berlin').format('YYYY-MM-DD');
+    }
+    return dateString;
+  }
+}
+
+export const SELECTED_DATE_SERVICE_PROVIDER = {
+  provide: SelectedDateService,
+  useClass: AbstractedSelectedDateService,
+};
