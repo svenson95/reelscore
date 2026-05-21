18cefdc33c16714a039d5f0dbe47106598b1a4f9
Sven Brodny <svenbrodny@googlemail.com>
2026-05-21 19:57:35 +0200

    style(competition-select): mat-option icon + label spacing

--- apps/client/src/app/shared/components/header/components/competition-select.component.ts
+++ apps/client/src/app/shared/components/header/components/competition-select.component.ts
@@ -65,7 +65,7 @@ import { OptimizedImageComponent } from '../../optimized-image/optimized-image.c
       --mat-option-label-text-size: var(--rs-font-size-body-3);
       @apply min-h-[32px] p-0;
 
-      a { @apply flex w-full h-full px-4 py-1 gap-2; }
+      a { @apply flex w-full h-full px-4 py-1 gap-4; }
       .competition-logo + span { @apply leading-[1.3] content-center; }
     }
   `,
