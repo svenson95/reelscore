7766e6a4acbb0abf9c07022fb554a127e4cad66a
Sven Brodny <svenbrodny@googlemail.com>
2026-05-21 05:24:13 +0200

    style(app): some updates

--- apps/client/src/app/modules/match/components/match-header/components/data.component.ts
+++ apps/client/src/app/modules/match/components/match-header/components/data.component.ts
@@ -28,8 +28,8 @@ import {
     .team-column { @apply flex-col gap-2 text-rs-font-size-body-2; }
     .result-column { @apply relative items-center justify-center gap-1 text-rs-font-size-body-1; }
     .status { @apply absolute top-0 py-[.15rem] px-2 text-rs-font-size-small font-mono; }
-    .status.is-playing { @apply bg-green-600 text-rs-color-text-3; }
-    .status.is-finished { @apply bg-gray-600 text-rs-color-text-3; }
+    .status.is-playing { @apply bg-green-600 text-white; }
+    .status.is-finished { @apply bg-gray-600 text-white; }
     .team-name { @apply leading-[16px] text-center; }
     .team-logo { @apply m-auto; }
     .team-name-placeholder { @apply w-[100px] h-[16px] bg-gray-200 rounded m-auto; }
