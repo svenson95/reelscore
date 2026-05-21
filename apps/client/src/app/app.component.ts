02cf864a2d5555db49d317ef4c452a966709c5ce
Sven Brodny <svenbrodny@googlemail.com>
2026-05-21 05:36:54 +0200

    revert(app): hide header if is router loading

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,28 +1,20 @@
-import {
-  AfterViewInit,
-  ChangeDetectionStrategy,
-  Component,
-  DestroyRef,
-  inject,
-  signal,
-  viewChild,
-} from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 import { FooterComponent, HeaderComponent } from './shared';
 
 @Component({
   selector: 'rs-root',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterModule, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   styles: `
     :host {
       box-sizing: border-box;
       -webkit-font-smoothing: antialiased;
       -moz-osx-font-smoothing: grayscale;
     }
-    rs-logo { 
+
+    rs-logo {
       transform: translate(-50%, -50%);
       position: absolute;
       top: 50%;
@@ -30,24 +22,11 @@ import { FooterComponent, HeaderComponent } from './shared';
     }
   `,
   template: `
-    @if(!isRouterLoading()) {
     <header rs-header-content></header>
-    }
     <main>
       <router-outlet />
     </main>
     <footer rs-footer-content></footer>
   `,
 })
-export class AppComponent implements AfterViewInit {
-  private outlet = viewChild.required<RouterOutlet>(RouterOutlet);
-  private destroyRef = inject(DestroyRef);
-
-  isRouterLoading = signal<boolean>(true);
-
-  public ngAfterViewInit(): void {
-    this.outlet()
-      ?.activateEvents.pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe(() => this.isRouterLoading.set(false));
-  }
-}
+export class AppComponent {}
