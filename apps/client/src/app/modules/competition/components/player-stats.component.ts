b4483a97017f2975551bf11c7718f6d905ba88f5
Sven Brodny <svenbrodny@googlemail.com>
2026-05-29 02:05:17 +0200

    style(competition): add top-scorers bg-color

--- apps/client/src/app/modules/competition/components/player-stats.component.ts
+++ apps/client/src/app/modules/competition/components/player-stats.component.ts
@@ -15,14 +15,16 @@ import { TopScorersStore } from '../store';
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [TeamNamePipe],
   styles: `
-    :host { @apply rs-competition-tab max-[700px]:flex-col gap-rs2 flex-wrap mx-2; }
+    :host { @apply flex max-[700px]:flex-col gap-rs2 flex-wrap; }
     .column { @apply flex-1; }
-    h2 { @apply mb-4 border-b border-rs-color-primary; }
-    .divider { @apply self-stretch min-[700px]:w-px max-[700px]:hidden bg-rs-color-primary; }
-    .player-stat { @apply flex justify-between; }
-    .player-stat:not(:last-child) { @apply mb-2; }
-    img { @apply inline-block w-[24px] h-[24px] mr-4 rounded-full; }
-    .team-name { @apply ml-4 text-rs-color-text-2 text-rs-font-size-body-2; }
+    h2 { @apply mb-2 ml-4; }
+    .divider { @apply mt-3 self-stretch min-[700px]:w-px max-[700px]:hidden bg-rs-color-primary; }
+    .player-stat { @apply flex justify-between bg-rs-alt-bg px-4 py-2 border border-rs-border-color-2; }
+    .player-stat:not(:last-child) { @apply mb-px; }
+    img { @apply inline-block w-[24px] h-[24px] mr-3 rounded-full; }
+    .team-name { @apply ml-3 text-rs-color-text-2 text-rs-font-size-body-3; }
+    .player-name { @apply text-rs-font-size-body-1; }
+    .stat-value { @apply text-rs-font-size-body-1 self-center; }
   `,
   template: `
     @if (topScorers() !== null) {
@@ -30,14 +32,14 @@ import { TopScorersStore } from '../store';
       <h2>Torschützen</h2>
       @for (stat of goalScorer(); track stat.player.id; let idx = $index) {
       <div class="player-stat">
-        <div class="player-name">
+        <div class="player-row">
           <img [src]="stat.player.photo" />
-          <span>{{ stat.player.name }}</span>
+          <span class="player-name">{{ stat.player.name }}</span>
           <span class="team-name">
             {{ stat.statistics[0].team.name | teamName : 'short' }}
           </span>
         </div>
-        <span>{{ stat.statistics[0].goals.total }}</span>
+        <span class="stat-value">{{ stat.statistics[0].goals.total }}</span>
       </div>
       }
     </div>
@@ -48,14 +50,14 @@ import { TopScorersStore } from '../store';
       <h2>Vorlagen</h2>
       @for (stat of assists(); track stat.player.id; let idx = $index) {
       <div class="player-stat">
-        <div class="player-name">
+        <div class="player-row">
           <img [src]="stat.player.photo" />
-          <span>{{ stat.player.name }}</span>
+          <span class="player-name">{{ stat.player.name }}</span>
           <span class="team-name">
             {{ stat.statistics[0].team.name | teamName : 'short' }}
           </span>
         </div>
-        <span>{{ stat.statistics[0].goals.assists }}</span>
+        <span class="stat-value">{{ stat.statistics[0].goals.assists }}</span>
       </div>
       }
     </div>
