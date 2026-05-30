3a7e4626823d4ca4dce4583307c88a5af4bbeb79
Sven Brodny <svenbrodny@googlemail.com>
2026-05-30 15:05:46 +0200

    refactor(standings): logic

--- apps/client/src/app/shared/helper/standings.helper.ts
+++ apps/client/src/app/shared/helper/standings.helper.ts
@@ -1,12 +1,10 @@
 import { StandingsDTO } from '@lib/models';
 import { isCompetitionWithMultipleGroups } from '@lib/shared';
 
-export const hasMultipleGroups = (standings: StandingsDTO | null) => {
-  if (!standings) return false;
+export const hasMultipleGroups = (standings: StandingsDTO): boolean => {
   return isCompetitionWithMultipleGroups(standings.league.id);
 };
 
-export const showHomeAndAwayStandings = (standings: StandingsDTO | null) => {
-  if (!standings) return false;
+export const showHomeAndAwayStandings = (standings: StandingsDTO): boolean => {
   return standings.league.standings?.length === 3;
 };
