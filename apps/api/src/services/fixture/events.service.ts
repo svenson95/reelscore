f472a8c0d704a9dc9a4d354d439d543c3dfe7780
Sven Brodny <svenbrodny@googlemail.com>
2026-05-31 01:23:57 +0200

    fix(match): highlights penalties missed shots

--- apps/api/src/services/fixture/events.service.ts
+++ apps/api/src/services/fixture/events.service.ts
@@ -1,5 +1,3 @@
-import mongoose from 'mongoose';
-
 import {
   EventDTO,
   EventTime,
@@ -13,27 +11,38 @@ import { FixtureEvents } from '../../models';
 export class FixtureEventsService {
   async findById(
     fixtureId: FixtureIdParameter
-  ): Promise<mongoose.FlattenMaps<RapidEventsDTO>> {
+  ): Promise<RapidEventsDTO | null> {
     const events = await findDocument(
       FixtureEvents,
       'parameters.fixture',
       fixtureId
     );
+
     return events;
   }
 
   filterHighlights(events: EventDTO[] | undefined): EventDTO[] {
     if (!events) return [];
+
     const goals = events.filter(({ type, detail }) => {
       // TODO refactor to lib (1/2)
-      const goalTypes = ['Normal Goal', 'Own Goal', 'Penalty'];
-      return type === 'Goal' && goalTypes.includes(detail);
+      const HIGHLIGHT_GOAL_TYPES = [
+        'Normal Goal',
+        'Own Goal',
+        'Penalty',
+        'Missed Penalty',
+      ];
+
+      return type === 'Goal' && HIGHLIGHT_GOAL_TYPES.includes(detail);
     });
+
     const redCards = events.filter(
       ({ type, detail }) => type === 'Card' && detail === 'Red Card'
     );
+
     return [...goals, ...redCards].sort((event1, event2) => {
       const time = (t: EventTime) => t.elapsed + (t.extra ?? 0);
+
       return time(event1.time) - time(event2.time);
     });
   }
