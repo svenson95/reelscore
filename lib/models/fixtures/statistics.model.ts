e2422086d60194be0841a36c9f18b681f82546c5
Sven Brodny <svenbrodny@googlemail.com>
2026-06-14 03:50:30 +0200

    refactor(match): statistics labels & related models

--- lib/models/fixtures/statistics.model.ts
+++ lib/models/fixtures/statistics.model.ts
@@ -5,13 +5,46 @@ export type StatisticsTeamDetails = {
   name: TeamName;
   logo: TeamLogo;
 };
-export type StatisticItemType = string;
+
+export type StatisticItemType =
+  | 'Ball Possession'
+  | 'Total Shots'
+  | 'Shots on Goal'
+  | 'Shots off Goal'
+  | 'Corner Kicks'
+  | 'Fouls'
+  | 'Goalkeeper Saves'
+  | 'Offsides'
+  | 'Yellow Cards'
+  | 'Red Cards'
+  | 'Total passes'
+  | 'Passes %';
+
+type RequiredStatisticKey =
+  | 'ballPossession'
+  | 'shotsTotal'
+  | 'shotsOnGoal'
+  | 'shotsOffGoal'
+  | 'cornerKicks'
+  | 'fouls'
+  | 'goalkeeperSaves'
+  | 'offsides'
+  | 'yellowCards'
+  | 'redCards';
+
+type OptionalStatisticKey = 'passesTotal' | 'passAccuracy';
+
+export type StatisticKey = RequiredStatisticKey | OptionalStatisticKey;
+
 export type StatisticItemValue = string | number | null;
+
 export type StatisticItem = {
   type: StatisticItemType;
   value: StatisticItemValue;
 };
-export type TeamStatistics = Array<StatisticItem>;
+
+export type TeamStatistics = StatisticItem[];
+
 export interface StatisticDTO {
   team: StatisticsTeamDetails;
   statistics: TeamStatistics;
