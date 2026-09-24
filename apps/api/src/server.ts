902d6e8747a720e6c619207a9330571fe5a4e1cd
Sven Brodny <svenbrodny@googlemail.com>
2026-09-24 17:15:27 +0200

    fix(ci): Vercel deployment setup (2)

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -2,10 +2,10 @@ import 'dotenv/config';
 
 import { app } from './app';
 
-const port = Number(process.env.API_PORT);
+const port = Number(process.env.PORT ?? process.env.API_PORT ?? 3000);
 
 const server = app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`[server]: Server is running on port ${port}`);
 });
 
 server.on('error', console.error);
